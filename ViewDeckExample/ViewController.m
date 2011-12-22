ab0ed29f0e984430f67fa47db77d0631e26ff392
Inferis <spam@inferis.org>
2011-12-22 01:24:13 +0100

    Allow all interface orientations in demo app.

--- ViewDeckExample/ViewController.m
+++ ViewDeckExample/ViewController.m
@@ -65,7 +65,7 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
 #pragma mark - Table view data source

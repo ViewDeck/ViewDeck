4a3ae48a0e4dd744c28426df1508bb0c8c52b757
Inferis <spam@inferis.org>
2012-01-13 15:24:11 +0100

    Added ARC detection.

--- ViewDeck/IIViewDeckController.m
+++ ViewDeck/IIViewDeckController.m
@@ -23,6 +23,28 @@
 //  SOFTWARE.
 //
 
+// define some LLVM3 macros if the code is compiled with a different compiler (ie LLVMGCC42)
+#ifndef __has_feature
+#define __has_feature(x) 0
+#endif
+#ifndef __has_extension
+#define __has_extension __has_feature // Compatibility with pre-3.0 compilers.
+#endif
+
+#if __has_feature(objc_arc) && __clang_major__ >= 3
+#define II_ARC_ENABLED 1
+#endif // __has_feature(objc_arc)
+
+#if II_ARC_ENABLED
+#define II_RETAIN(xx)  (id)(xx)
+#define II_RELEASE(xx)  (id)(xx)
+#define II_AUTORELEASE(xx)  (id)(xx)
+#else
+#define II_RETAIN(xx)           [xx retain]
+#define II_RELEASE(xx)          [xx release]
+#define II_AUTORELEASE(xx)      [xx autorelease]
+#endif
+
 
 #import "IIViewDeckController.h"
 #import <objc/runtime.h>

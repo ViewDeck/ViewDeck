1e6c535a113704108c70d4aa91c9ec573ad98a78
Inferis <spam@inferis.org>
2012-01-13 17:01:05 +0100

    Added property to change rotation behavior.

--- ViewDeck/IIViewDeckController.h
+++ ViewDeck/IIViewDeckController.h
@@ -49,6 +49,12 @@ typedef enum {
 } IIViewDeckNavigationControllerBehavior;
 
 
+typedef enum {
+    IIViewDeckRotationKeepsLedgeSizes, // when rotating, the ledge sizes are kept (side views are more/less visible)
+    IIViewDeckRotationKeepsViewSizes  // when rotating, the size view sizes are kept (ledges change)
+} IIViewDeckRotationBehavior;
+
+
 #define IIViewDeckCenterHiddenCanTapToClose(interactivity) ((interactivity) == IIViewDeckCenterHiddenNotUserInteractiveWithTapToClose || (interactivity) == IIViewDeckCenterHiddenNotUserInteractiveWithTapToCloseBouncing)
 #define IIViewDeckCenterHiddenIsInteractive(interactivity) ((interactivity) == IIViewDeckCenterHiddenUserInteractive)
 
@@ -68,6 +74,7 @@ typedef enum {
 @property (nonatomic) IIViewDeckPanningMode panningMode;
 @property (nonatomic) IIViewDeckCenterHiddenInteractivity centerhiddenInteractivity;
 @property (nonatomic) IIViewDeckNavigationControllerBehavior navigationControllerBehavior;
+@property (nonatomic) IIViewDeckRotationBehavior rotationBehavior;
 
 - (id)initWithCenterViewController:(UIViewController*)centerController;
 - (id)initWithCenterViewController:(UIViewController*)centerController leftViewController:(UIViewController*)leftController;

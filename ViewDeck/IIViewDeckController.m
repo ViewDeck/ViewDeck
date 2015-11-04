2593fc13db28ff96181feb67ba8cf383704a2801
Matthew Abbott <abbottmg@me.com>
2015-11-04 13:31:26 -0600

    Add BOOLs to avoid calls that would throw
    It's still best to wrap the calls in @try, but this bookkeeping prevents us from running headlong into an exception we *know* will be thrown.

--- ViewDeck/IIViewDeckController.m
+++ ViewDeck/IIViewDeckController.m
@@ -183,6 +183,8 @@ static NSTimeInterval durationToAnimate(CGFloat pointsToAnimate, CGFloat velocit
 @property (nonatomic, retain) UIButton* centerTapper;
 @property (nonatomic, retain) UIView* centerView;
 @property (nonatomic, readonly) UIView* slidingControllerView;
+@property (nonatomic, assign) BOOL isObservingView;
+@property (nonatomic, assign) BOOL isObservingSelf;
 
 - (void)cleanup;
 - (uint)sideControllerCount;
@@ -459,16 +461,22 @@ static NSTimeInterval durationToAnimate(CGFloat pointsToAnimate, CGFloat velocit
     self.panners = nil;
     
     // observations related to UIViewController properties
-    @try {
-        [self removeObserver:self forKeyPath:@"parentViewController"];
-        [self removeObserver:self forKeyPath:@"presentingViewController"];
-    } @catch(id anException) {
-        
+    if (self.isObservingSelf) {
+        @try {
+            [self removeObserver:self forKeyPath:@"parentViewController"];
+            [self removeObserver:self forKeyPath:@"presentingViewController"];
+            self.isObservingSelf = NO;
+        } @catch(id anException) {
+            
+        }
     }
-    @try {
-        [self.view removeObserver:self forKeyPath:@"bounds"];
-    } @catch(id anException) {
-    
+    if (self.isObservingView) {
+        @try {
+            [self.view removeObserver:self forKeyPath:@"bounds"];
+            self.isObservingView = NO;
+        } @catch(id anException) {
+            
+        }
     }
 #if !II_ARC_ENABLED
     [super dealloc];
@@ -957,6 +965,7 @@ static NSTimeInterval durationToAnimate(CGFloat pointsToAnimate, CGFloat velocit
     [super viewWillAppear:animated];
     
     [self.view addObserver:self forKeyPath:@"bounds" options:NSKeyValueObservingOptionNew context:nil];
+    self.isObservingView = YES;
 
     if (!_viewFirstAppeared) {
         _viewFirstAppeared = YES;
@@ -1052,10 +1061,13 @@ static NSTimeInterval durationToAnimate(CGFloat pointsToAnimate, CGFloat velocit
     if (self.navigationControllerBehavior == IIViewDeckNavigationControllerIntegrated)
         _willAppearOffset = self.slidingControllerView.frame.origin;
 
-    @try {
-        [self.view removeObserver:self forKeyPath:@"bounds"];
-    } @catch(id anException){
-        //do nothing, obviously it wasn't attached because an exception was thrown
+    if (self.isObservingView) {
+        @try {
+            [self.view removeObserver:self forKeyPath:@"bounds"];
+            self.isObservingView = NO;
+        } @catch(id anException){
+            //do nothing, obviously it wasn't attached because an exception was thrown
+        }
     }
     
     if ([self safe_shouldManageAppearanceMethods]) {
@@ -3282,6 +3294,7 @@ static NSTimeInterval durationToAnimate(CGFloat pointsToAnimate, CGFloat velocit
             II_RETAIN(_finishTransitionBlocks);
             [self addObserver:self forKeyPath:@"parentViewController" options:0 context:nil];
             [self addObserver:self forKeyPath:@"presentingViewController" options:0 context:nil];
+            self.isObservingSelf = YES;
         }
         [_finishTransitionBlocks addObject:finishTransition];
     }
@@ -3291,8 +3304,15 @@ static NSTimeInterval durationToAnimate(CGFloat pointsToAnimate, CGFloat velocit
     if (!self.referenceView) return;
     
     if (_finishTransitionBlocks) {
-        [self removeObserver:self forKeyPath:@"parentViewController" context:nil];
-        [self removeObserver:self forKeyPath:@"presentingViewController" context:nil];
+        if (self.isObservingSelf) {
+            @try {
+                [self removeObserver:self forKeyPath:@"parentViewController" context:nil];
+                [self removeObserver:self forKeyPath:@"presentingViewController" context:nil];
+                self.isObservingSelf = NO;
+            } @catch (id anException) {
+                
+            }
+        }
         
         for (void(^finishTransition)(void) in _finishTransitionBlocks) {
             finishTransition();
